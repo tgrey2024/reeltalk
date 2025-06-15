efcac94ad037fb296aa0f158d2990143fda1cff4
tgrey2024 <tripta.grey@gmail.com>
2025-06-15 22:07:50 +0100

    refactor Button and Alert components to manage alert visibility and add close functionality

--- reeltalk/src/App.tsx
+++ reeltalk/src/App.tsx
@@ -1,11 +1,16 @@
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
+  const [showAlert, setShowAlert] = useState(false);
+
   return (
     <div>
-      <Alert>Hello World</Alert>
-      <Button onClick={() => console.log("Clicked")}>Click me!</Button>
+      {showAlert && (
+        <Alert onClose={() => setShowAlert(false)}>Hello World</Alert>
+      )}
+      <Button onClick={() => setShowAlert(true)}>Click me!</Button>
     </div>
   );
 }
